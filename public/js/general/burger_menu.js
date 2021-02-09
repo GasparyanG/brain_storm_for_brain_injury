02c2e8f4bcfc7d49f1e8a01f3236f4daeebf6d5d
Garnik Gasparyan <gasp.garnik@gmail.com>
2021-02-09 17:20:56 +0400

    Handle page resizing problems

--- public/js/general/burger_menu.js
+++ public/js/general/burger_menu.js
@@ -3,8 +3,22 @@ const navigationMenu = document.querySelector(".nav_items");
 
 function openNavigationMenu(e) {
     navigationMenu.classList.toggle("nav_items_opened");
+    if (navigationMenu.classList.contains("nav_items_opened"))
+        document.body.style.setProperty("overflow-y", "hidden");
+    else
+        document.body.style.setProperty("overflow-y", "unset");
 }
 
+function resetToBigScreen() {
+    navigationMenu.classList.remove("nav_items_opened");
+    document.body.style.setProperty("overflow-y", "unset");
+}
+
+function handleMenuBar(e) {
+    if (window.innerWidth > 750)
+        resetToBigScreen();
+}
 
 // Add Events.
 burgerMenuIcon.addEventListener("click", openNavigationMenu);
+window.onresize = handleMenuBar;
