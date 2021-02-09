829fcdd9491eb3ec2a1bed0f86c53c5f4efe1236
Garnik Gasparyan <gasp.garnik@gmail.com>
2021-02-09 17:49:32 +0400

    Design navigation items and background further

--- public/js/general/burger_menu.js
+++ public/js/general/burger_menu.js
@@ -1,5 +1,6 @@
 const burgerMenuIcon = document.querySelector(".burger_menu");
 const navigationMenu = document.querySelector(".nav_items");
+const payloadElement = document.getElementById("payload");
 
 function openNavigationMenu(e) {
     navigationMenu.classList.toggle("nav_items_opened");
@@ -7,9 +8,12 @@ function openNavigationMenu(e) {
         document.body.style.setProperty("overflow-y", "hidden");
     else
         document.body.style.setProperty("overflow-y", "unset");
+
+    payloadElement.classList.toggle("blur_payload");
 }
 
 function resetToBigScreen() {
+    payloadElement.classList.remove("blur_payload");
     navigationMenu.classList.remove("nav_items_opened");
     document.body.style.setProperty("overflow-y", "unset");
 }
