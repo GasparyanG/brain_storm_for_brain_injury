0ccb19b18ce984187db19838e3a427a7394382de
Garnik Gasparyan <gasp.garnik@gmail.com>
2021-02-09 11:23:02 +0400

    Implement some DOM manipulation

--- public/js/thank_you/email_section.js
+++ public/js/thank_you/email_section.js
@@ -1,4 +1,12 @@
 const emailInputField = document.getElementById("email");
+const actionBox = document.querySelector(".action_box");
+const notificationBox = document.querySelector(".notification_box");
+
+// CONFIGURATIONS
+const EmailMessages = {
+    email_wrong_format: "Wrong email format",
+    success: "Got it!",
+}
 
 // Helper functions.
 function validateEmail(email) {
@@ -7,36 +15,65 @@ function validateEmail(email) {
 }
 
 // DOM Manipulation Functions.
-function displayEmailSendButton() {
+function clearNotificationContainer() {
+    notificationBox.innerHTML = "";
+}
 
+function displayEmailSendButton() {
+    actionBox.classList.remove("action_box_disabled");
 }
 
 function hideEmailSendButton() {
-
+    actionBox.classList.add("action_box_disabled");
 }
 
 function displayEmailError(message) {
+    let divElement = document.createElement("div");
+    divElement.classList.add("error_message");
+    divElement.textContent = message;
 
+    // Add to notification container.
+    clearNotificationContainer();
+    notificationBox.appendChild(divElement);
 }
 
 function displayEmailSuccess(message) {
+    let divElement = document.createElement("div");
+    divElement.classList.add("success_message");
+    divElement.textContent = message;
 
+    // Add to notification container.
+    clearNotificationContainer();
+    notificationBox.appendChild(divElement);
 }
 
+// TODO: Notify the user about verification urgency.
 function displayEmailInformation(message) {
 
 }
 
+function validMeasures() {
+    displayEmailSendButton();                                           // Display button.
+    clearNotificationContainer();                                       // Remove error message.
+}
+
+function invalidMeasures() {
+    hideEmailSendButton();                                              // Hide button.
+    displayEmailError(EmailMessages.email_wrong_format);                // Display error.
+}
+
 // Functions to Handle Events.
 function emailValidation(e) {
-    if (validateEmail(e.target.value)) console.log("valid!");
-    else console.log("invalid!");
+    if (validateEmail(e.target.value)) validMeasures();
+    else invalidMeasures();
 }
 
 
 function sendEmail(e) {
     if (e.keyCode !== 13) return;
-    console.log(e.target.value);
+    if (!validateEmail(e.target.value)) invalidMeasures();
+
+    // Send Ajax POST Request.
 }
 
 // Adding events.
